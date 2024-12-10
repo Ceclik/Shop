9147099adadc78e294c1c01d639ea52bfa483252
Ceclik <yura11122345@gmail.com>
2024-12-10 21:43:15 +0300

    Death menu added

---
+++ Assets/Scripts/Components/ButtonsHandler.cs
@@ -0,0 +1,14 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+
+namespace Components
+{
+    public class ButtonsHandler : MonoBehaviour
+    {
+        public void OnRestartOrPlayButtonClick()
+        {
+            Time.timeScale = 1f;
+            SceneManager.LoadScene("Gameplay");
+        }
+    }
+}
---
+++ Assets/Scripts/Components/CharacterComponents/CharacterDeathHandler.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using UnityEngine;
+
+namespace Components.CharacterComponents
+{
+    public class CharacterDeathHandler : MonoBehaviour
+    {
+        [SerializeField] private int amountOfTimersFailedToDeath;
+        [SerializeField] private GameObject deathPanel;
+
+        private TimerHandler _timer;
+        private int _amountOfFailedTimers;
+
+        private void Start()
+        {
+            _timer = GameObject.FindGameObjectWithTag("Timer").GetComponent<TimerHandler>();
+            _timer.OnTimerFailed += FailedTimersCounter;
+        }
+
+        private void FailedTimersCounter()
+        {
+            _amountOfFailedTimers++;
+
+            if (_amountOfFailedTimers >= amountOfTimersFailedToDeath)
+            {
+                deathPanel.SetActive(true);
+                Time.timeScale = 0f;
+                Cursor.lockState = CursorLockMode.Confined;
+            }
+        }
+
+        private void OnDestroy()
+        {
+            _timer.OnTimerFailed -= FailedTimersCounter;
+        }
+    }
+}
